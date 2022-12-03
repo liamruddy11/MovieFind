83ed590cf339d398d55bd03d02dec71888171a7a
liamruddy <ruddyli@bc.edu>
2022-12-03 15:40:44 -0500

    Initial Commit

---
+++ MovieFind/MovieFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFindApp.swift
+//  MovieFind
+//
+//  Created by Liam Ruddy on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
